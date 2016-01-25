8315fb8ee75b7fa26e552454de9b244449a34b08
NitishDhakal <nitish.dhakal@gmail.com>
2016-01-24 20:48:52 -0700

    Cache matrix implementation
    Cache matrix implementation with create matrix and inverse matrix
    functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cache function to cache the inverse of the matrix
+## Usually generating single inverse of a matrix may result in faster computational time.
+## Larger datasets involving large number of inverse calculations however will be slow.
+## The functions written below creates a matrix and caches its inverse.
 
-## Write a short comment describing this function
+##This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        matInverse <-NULL;
+        setMatrix<-function(matrix){
+                x<<-matrix;
+                matInverse<<-NULL
+                
+        }
+        getMatrix<-function() x
+        setInverse<-function(matrixInverse) matInverse<<-matrixInverse;
+        getInverse<-function() matInverse;
+        list(getMatrix=getMatrix,setMatrix=setMatrix,setInverse=setInverse,getInverse=getInverse);
+        
 }
 
 
-## Write a short comment describing this function
-
+## This function takes the matrix generated above 
+## and calculates its inverse if not calculated earlier 
+## behaving like a cache memory of the inverse matrix.
+## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inverseMatrix=x$getInverse();
+        #if the inverseMatrix is already generated then get the data
+        #instead of again calculating it
+        if (!is.null(inverseMatrix)){
+                message("Getting cached data")
+                return(inverseMatrix)
+        }
+        matrixGet<-x$getMatrix();
+        inverseOfMatrix <- solve(matrixGet);
+        x$setInverse(inverseOfMatrix);
+        inverseOfMatrix;
 }
